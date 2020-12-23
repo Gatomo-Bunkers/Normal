fb9bc87789eb4eda900aa0af4ade9744eab88ea4
Gátomo <Gatomociencia@gmail.com>
2020-12-23 23:35:30 +0100

    Update avatar.js

--- src/commands/General/avatar.js
+++ src/commands/General/avatar.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
 	}
 
 	async run(msg, [user = msg.author]) {
-		const avatar = user.displayAvatarURL({ size: 512 });
+		const avatar = user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 });
 
 		return msg.sendEmbed(new MessageEmbed()
 			.setAuthor(user.username, avatar)
